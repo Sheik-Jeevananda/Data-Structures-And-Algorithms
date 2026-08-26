d38d6efa3a7fd4980ccd8a135ac893069bf2a0bc
Sheik Jeevananda <jeevananda359@gmail.com>
2026-08-26 23:07:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (39.85%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == divisor) return 1;
+
+        bool sign = true;
+
+        if ((divisor >= 0 && dividend < 0) ||
+            (divisor < 0 && dividend >= 0)) {
+            sign = false;
+        }
+
+        long long n = llabs((long long)dividend);
+        long long d = llabs((long long)divisor);
+
+        long long quotient = 0;
+
+        while (n >= d) {
+            int cnt = 0;
+
+            while (n >= (d << (cnt + 1))) {
+                cnt++;
+            }
+
+            quotient += (1LL << cnt);
+            n -= (d << cnt);
+        }
+
+        if (quotient > INT_MAX) {
+            return sign ? INT_MAX : INT_MIN;
+        }
+
+        return sign ? quotient : -quotient;
+    }
+};
